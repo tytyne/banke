07c3e760c8b4eb50b231fc8b265a23f438e76eb6
tytyne <dusaflora2@gmail.com>
2019-08-05 10:20:13 +0100

    make changes

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -1,63 +1,9 @@
 'use strict'
 
-// const { test } = use('Test/Suite')('User')
+const { test } = use('Test/Suite')('User')
 
-// test('make sure 2 + 2 is 4', async ({ assert }) => {
-//   assert.equal(2 + 2, 4)
-// })
-
-
-const Factory = use('Factory');
-const {test, trait} = use('Test/Suite')('Accounts')
-
-trait('Test/ApiClient')
-trait('Auth/Client')
-
-
-test('can create account', async ({assert, client}) => {
-
-  const user = await Factory.model('App/Models/User').create()
-
-  const data = {
-      NID: 'Test NID',
-      type: 'Test type'
-
-  };
-
-  const response = await client.account('/accounts')
-    .loginVia(user, 'jwt')
-    .send(data).end()
-
-  response.assertStatus(200);
-  response.assertJSONSubset({
-    NID: data.NID,
-    type: data.type,
-    user_id: user.id,
-    accountName:user.name,
-  })
+test('make sure 2 + 2 is 4', async ({ assert }) => {
+  assert.equal(2 + 2, 4)
 })
-
-test('can list account', async ({assert, client}) => {
-
-  const user = await Factory.model('App/Models/User').create()
-  const account = await Factory.model('App/Models/Account').make()
-
-  await user.accounts().save(account)
-  const response = await client.get('/accounts').end()
-
-
-  response.assertStatus(200);
-
-  response.assertJSONSubset(
-    [{
-      NID: account.NID,
-      type: account.type,
-      user_id: user.id,
-      accountName:user.name,
-    }]
-  )
-
-})
-
 
 
