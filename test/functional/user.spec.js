8bce5bc4709de71bac72339083a08a14bdbd7683
tytyne <dusaflora2@gmail.com>
2019-08-06 08:48:09 +0100

    make changes

--- test/functional/user.spec.js
+++ test/functional/user.spec.js
@@ -1,4 +1,19 @@
+const { test, trait } = use('Test/Suite')('Account')
+const Account = use('App/Models/Account')
 
+trait('Test/ApiClient')
 
+test('get list of accounts', async ({ client }) => {
+  await Account.create({
+    NID: '123',
+    type: 'saving'
+  })
 
+  const response = await client.get('/accounts').end()
 
+  response.assertStatus(200)
+  response.assertJSONSubset([{
+    NID: '123',
+    type: 'saving'
+  }])
+})
